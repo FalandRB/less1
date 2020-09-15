155a75eec73513393e96a5c3dffbe73082ec3dce
FalandRB <70664505+FalandRB@users.noreply.github.com>
2020-09-15 21:43:58 +0300

    Update bmi.py

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -4,37 +4,35 @@ weight = float(input('Ваш вес в килограммах: '))
 # расчет
 bmi = weight / height**2
 # для красоты можно сделать round(bmi) и сделать print('Ваш индекс массы тела:', round(bmi)) или распишем состояние
-if bmi < 16:
-   print('Ваш индекс массы тела:', round(bmi), 'Выраженный дефицит массы тела')
+if bmi >= 1 and bmi < 16:
+    print('Ваш индекс массы тела:', round(bmi),
+          'Выраженный дефицит массы тела')
 elif bmi >= 16 and bmi < 18.5:
-   print('Ваш индекс массы тела:', round(bmi), 'Недостаточная (дефицит) масса тела')
+    print('Ваш индекс массы тела:', round(bmi),
+          'Недостаточная (дефицит) масса тела')
 elif bmi >= 18.5 and bmi < 25:
-   print('Ваш индекс массы тела:', round(bmi), 'Норма')
+    print('Ваш индекс массы тела:', round(bmi), 'Норма')
 elif bmi >= 25 and bmi < 30:
-   print('Ваш индекс массы тела:', round(bmi), 'Избыточная масса тела (предожирение)')
+    print('Ваш индекс массы тела:', round(bmi),
+          'Избыточная масса тела (предожирение)')
 elif bmi >= 30 and bmi < 35:
-   print('Ваш индекс массы тела:', round(bmi), 'Ожирение')
+    print('Ваш индекс массы тела:', round(bmi), 'Ожирение')
 elif bmi >= 35 and bmi < 40:
-   print('Ваш индекс массы тела:', round(bmi), 'Ожирение резкое')
-elif bmi >= 40:
-   print('Ваш индекс массы тела:', round(bmi), 'Очень резкое ожирение')
-# Строим график от 0 до 100, теоретически такое может быть.
-first = round(bmi)
-second = 100 - first
-scale = '0' + '=' * (first - 1) + str(first) + '=' * (second - 1) + '100'
-# Тут красивее именно так '=' * (first - 1) и '=' * (second - 1)
-print('График (0 - 100): \n', scale)
-"""
-В целом можно предусмотреть вывод относительной шкалы,
-но нужно предусмотреть крайние точки и шаг.
-Всё равно очень много условностей.
-Лучшее враг хорошего ;)
-"""
+    print('Ваш индекс массы тела:', round(bmi), 'Ожирение резкое')
+elif bmi >= 40 and bmi < 100:
+    print('Ваш индекс массы тела:', round(bmi), 'Очень резкое ожирение')
+else:
+   exit('Введены неверные значения, попробуйте еще раз')
+# Считаем и выводим шкалу
 print('Построим шаговую шкалу')
 step_beg = int(input('Начало шкалы: '))
 step_end = int(input('Конец шкалы: '))
-step = int(input('Шаг шкалы: '))
-first = round(bmi) - step_beg
-second = step_end - round(bmi)
-scale = str(step_beg) + '=' * (round(first / step) - 1) + str(round(bmi)) + '=' * round((second / step) - 1) + str(step_end)
+if step_beg > round(bmi) or step_end <  round(bmi):
+   exit('Введены неверные значения, попробуйте еще раз')
+else:
+   step = int(input('Шаг шкалы: '))
+   first = round(bmi) - step_beg
+   second = step_end - round(bmi)
+   scale = str(step_beg) + '=' * (round(first / step) - 1) + \
+       str(round(bmi)) + '=' * round((second / step) - 1) + str(step_end)
 print('График: \n', scale)
