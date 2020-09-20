1c18728216363f89393604fa5f30050bdb922f83
FalandRB <70664505+FalandRB@users.noreply.github.com>
2020-09-20 14:46:40 +0300

    Update bmi.py

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -1,40 +1,83 @@
 # ввод данных
-height = float(input('Ваш рост в сантиметрах: ')) / 100
-weight = float(input('Ваш вес в килограммах: '))
-if weight <= 0 and height <= 0:
-   exit('Введены неверные значения, попробуйте еще раз')
-# расчет
-bmi = weight / height**2
-# для красоты можно сделать round(bmi) и сделать print('Ваш индекс массы тела:', round(bmi)) или распишем состояние
-if bmi >= 1 and bmi < 16:
-    print('Ваш индекс массы тела:', round(bmi),
-          'Выраженный дефицит массы тела')
-elif bmi >= 16 and bmi < 18.5:
-    print('Ваш индекс массы тела:', round(bmi),
-          'Недостаточная (дефицит) масса тела')
-elif bmi >= 18.5 and bmi < 25:
-    print('Ваш индекс массы тела:', round(bmi), 'Норма')
-elif bmi >= 25 and bmi < 30:
-    print('Ваш индекс массы тела:', round(bmi),
-          'Избыточная масса тела (предожирение)')
-elif bmi >= 30 and bmi < 35:
-    print('Ваш индекс массы тела:', round(bmi), 'Ожирение')
-elif bmi >= 35 and bmi < 40:
-    print('Ваш индекс массы тела:', round(bmi), 'Ожирение резкое')
-elif bmi >= 40 and bmi < 100:
-    print('Ваш индекс массы тела:', round(bmi), 'Очень резкое ожирение')
-else:
-   exit('Введены неверные значения, попробуйте еще раз')
-# Считаем и выводим шкалу
-print('Построим шаговую шкалу')
-step_beg = int(input('Начало шкалы: '))
-step_end = int(input('Конец шкалы: '))
-if step_beg > round(bmi) or step_end <  round(bmi):
-   exit('Введены неверные значения, попробуйте еще раз')
-else:
-   step = int(input('Шаг шкалы: '))
-   first = round(bmi) - step_beg
-   second = step_end - round(bmi)
-   scale = str(step_beg) + '=' * (round(first / step) - 1) + \
-       str(round(bmi)) + '=' * round((second / step) - 1) + str(step_end)
-print('График: \n', scale)
+person_quant = int(input('Сколько человек? '))
+d = {}
+for i in range(person_quant):
+    name = input('Ваше имя: ')
+    d[name] = {'Пол': input('Ваш пол (муж/жен): '),
+               'Возраст': int(input('Ваш возраст (полных лет): ')),
+               'Рост': float(input('Ваш рост в сантиметрах: ')),
+               'Вес': float(input('Ваш вес в килограммах: '))
+               }
+    d[name]['Индекс'] = bmi = d[name]['Вес'] / (d[name]['Рост'] / 100) ** 2
+    print('Ваш индекс массы тела:', round(bmi))
+    # Дикое ветвление от условий
+    if d[name]['Пол'] == 'муж':
+        if d[name]['Возраст'] <= 14:
+            if bmi <= 19:
+                print(name, 'кушай кашу!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм парень!')
+            elif bmi > 30:
+                print(name, 'не налегай на конфеты!')
+            else:
+                pass
+        if d[name]['Возраст'] > 14 and d[name]['Возраст'] <= 40:
+            if bmi <= 19:
+                print(name, 'ешь мясо, пей пиво!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм, мужик!')
+            elif bmi > 30:
+                print(name, 'не налегай на жирное!')
+            else:
+                pass
+        if d[name]['Возраст'] > 40:
+            if bmi <= 19:
+                print(name, 'ешь мясо, пей пиво!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм дядя!')
+            elif bmi > 30:
+                print(name, 'не налегай на жирное!')
+            else:
+                pass
+    elif d[name]['Пол'] == 'жен':
+        if d[name]['Возраст'] <= 14:
+            if bmi <= 19:
+                print(name, 'кушай кашу!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм девочка!')
+            elif bmi > 30:
+                print(name, 'не налегай на конфеты!')
+            else:
+                pass
+        if d[name]['Возраст'] > 14 and d[name]['Возраст'] <= 40:
+            if bmi <= 19:
+                print(name, 'ешь мясо, пей пиво!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм, женщина!')
+            elif bmi > 30:
+                print(name, 'не налегай на жирное!')
+            else:
+                pass
+        if d[name]['Возраст'] > 40:
+            if bmi <= 19:
+                print(name, 'ешь мясо, пей пиво!')
+            elif bmi > 19 and bmi <= 30:
+                print(name, 'всё норм тётя!')
+            elif bmi > 30:
+                print(name, 'не налегай на жирное!')
+            else:
+                pass
+    else:
+        print(name, ', что ты такое????')
+        pass
+    # Считаем и выводим шкалу
+    print('Построим шаговую шкалу')
+    step_beg = int(input('Начало шкалы: '))
+    step_end = int(input('Конец шкалы: '))
+    step = int(input('Шаг шкалы: '))
+    first = round(bmi) - step_beg
+    second = step_end - round(bmi)
+    d[name]['Шкала'] = scale = str(step_beg) + '=' * (round(first / step) - 1) + \
+        str(round(bmi)) + '=' * round((second / step) - 1) + str(step_end)
+    print('Шкала: \n', scale)
+print(d)
