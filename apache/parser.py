661cf8dce26b2d0f93fab06cccb9d3bdb00bd337
FalandRB <70664505+FalandRB@users.noreply.github.com>
2020-09-28 00:09:00 +0300

    Update parser.py

--- apache/parser.py
+++ apache/parser.py
@@ -1,16 +1,25 @@
 import re
 from collections import Counter
 
-
+#Функция чтения
 def reader(filename):
+    #Патерны
     ip_exp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    #Не успеваю дойти до RE конкретного браузера, но в целом логика такая
+    br_exp = r'"."(.*?)" '
+    #Читаем файл
     with open(filename) as f:
         log = f.read()
         ips = re.findall(ip_exp, log)
-        count = Counter(ips)
+        browsers = re.findall(br_exp, log)
+        count_ip = Counter(ips)
+        count_br = Counter(browsers)
         print('Всего обращений:', len(ips))
-        print('Уникальных IP:', len(count))
-    return ips
+        print('Уникальных IP:', len(count_ip))
+        print('Все UserAgents:', browsers)
+        print('От каждого UA:', len(count_br))
+    #Вернем что надо, если надо
+    return True
 
-
+#Запуск функции
 reader('123.log')
